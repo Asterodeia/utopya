c69dc19d1da7530945a6721ff71b1be85e110651
Audrey Lievremont <asterodeia@gmail.com>
2016-09-26 21:57:46 +0200

    correctif script SQL : clé étrangère

--- database/migrations/2016_09_26_185700_create-persos-table.php
+++ database/migrations/2016_09_26_185700_create-persos-table.php
@@ -15,7 +15,8 @@ class CreatePersosTable extends Migration
     {
         Schema::create('persos', function (Blueprint $table) {
             $table->increments('id');
-			$table->integer('user_id');
+			$table->integer('user_id')->unsigned();
+			$table->foreign('user_id')->references('id')->on('users');
 			$table->string('name')->unique();
 			$table->string('race');
             $table->timestamps();
