7e96fb574f66254aa3de9975b79f4e48c2bfa9f8
Audrey Lievremont <asterodeia@gmail.com>
2016-10-14 18:09:58 +0200

    Ajout de personnage OK Création du controller Persos

---
+++ app/Http/Controllers/PersoController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+use App\Http\Requests;
+use App\Perso;
+
+
+class PersoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('persos', [
+            'persos' => Perso::orderBy('created_at', 'asc')->get()
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $perso = new Perso;
+        $perso->user_id = 1;
+        $perso->name = $request->input('name');
+        $perso->race = $request->input('race');
+
+        Log::info('Ajout du perso', ['perso' => $perso]);
+        $perso->save();
+        return redirect('/persos');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -10,37 +10,7 @@
 | to using a Closure or controller method. Build something great!
 |
 */
-use App\Perso;
 use Illuminate\Http\Request;
+use App\Http\Controllers\PersoController;
 
-Route::get('/', function () {
-  return view('persos', [
-    'persos' => Perso::orderBy('created_at', 'asc')->get()
-  ]);
-});
-
-Route::post('/perso', function (Request $request) {
-  $validator = Validator::make($request->all(), [
-      'name' => 'required|max:255',
-  ]);
-
-  if ($validator->fails()) {
-      return redirect('/')
-          ->withInput()
-          ->withErrors($validator);
-  }
-
-  $perso = new Perso;
-  $perso->user_id = 1;
-  $perso->name = $request->name;
-  $perso->race = $request->race;
-
-  return redirect('/');
-
-});
-
-Route::delete('/perso/{id}', function ($id) {
-  Perso::findOrFail($id)->delete();
-
-  return redirect('/');
-});
+Route::resource('persos', 'PersoController');
